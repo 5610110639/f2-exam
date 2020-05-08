2fb4dd2c9efd229a2edaa9a758595bf50e76002a
Angkhana Chuchuaypan <39267875+5610110639@users.noreply.github.com>
2020-05-08 16:10:42 +0700

    initial commit

---
+++ MyApp02.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+import javax.swing.JButton; 
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent;
+public class MyApp02 {
+	public static void main(final String[] args) {
+		final JFrame frame = new JFrame("My App");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 200);
+		final JButton button = new JButton("Click Me");
+		button.addActionListener(new MyActionListener());
+		frame.add(button);
+		
+		frame.setVisible(true);
+	}
+}
+class MyActionListener implements ActionListener{
+	public void actionPerformed(ActionEvent e){
+		System.out.println("Hello");
+	}
+}
