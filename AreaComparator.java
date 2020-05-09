ee68cd1688f3662b8da559560bbd32ec55d169cb
Angkhana Chuchuaypan <39267875+5610110639@users.noreply.github.com>
2020-05-09 15:41:38 +0700

    Delete AreaComparator.java

--- AreaComparator.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class AreaComparator implements Comparator<Shape>{
-	public int compare(Shape s1, Shape s2){
-		return new Double(s1.getArea()).compareTo(s2.getArea());
-	}
-}
